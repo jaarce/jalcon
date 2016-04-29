129d20cd67ea1172b220402bfec3ba43e5a3a646
JA Arce <ja@directworksmedia.com>
2016-04-30 07:45:36 +0800

    Update to 0.0.3

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from distutils.sysconfig import get_python_lib
 from setuptools import find_packages, setup
 
 
-if "install" in sys.argv:
+if 'install' in sys.argv:
     lib_paths = [get_python_lib()]
-    if lib_paths[0].startswith("/usr/lib/"):
+    if lib_paths[0].startswith('/usr/lib/'):
         # We have to try also with an explicit prefix of /usr/local in order to
         # catch Debian's custom user site-packages directory.
-        lib_paths.append(get_python_lib(prefix="/usr/local"))
+        lib_paths.append(get_python_lib(prefix='/usr/local'))
     for lib_path in lib_paths:
-        existing_path = os.path.abspath(os.path.join(lib_path, "jalcon"))
+        existing_path = os.path.abspath(os.path.join(lib_path, 'jalcon'))
         if os.path.exists(existing_path):
             # We note the need for the warning here, but present it after the
             # command is run, so it's more likely to be seen.
@@ -27,15 +27,16 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "jalcon",
-    version = "0.0.2",
-    author = "JA Arce",
-    author_email = "johnandrewarce@gmail.com",
-    description = ("jalcon is a bootstrap framework for Falcon with MongoDB"),
-    license = "BSD",
-    keywords = "jalcon falcon framework jalcon-rest",
-    url = "https://github.com/jaarce/jalcon",
+    name = 'jalcon',
+    version = '0.0.3',
+    author = 'JA Arce',
+    author_email = 'johnandrewarce@gmail.com',
+    description = ('jalcon is a bootstrap framework for Falcon with MongoDB'),
+    license = 'BSD',
+    keywords = 'jalcon falcon framework jalcon-rest',
+    url = 'https://github.com/jaarce/jalcon',
     packages=find_packages(),
+    data_files = [('', ['LICENSE.txt', 'README.md'])]
     scripts=['jalcon-admin.py'],
     long_description='Why should it be long when there\'s a short description. Simply JAmazing!',
     zip_safe=False,
