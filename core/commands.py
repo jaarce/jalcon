f9bac65b30faaf4c094b4da23f6b8969f42c629e
JA Arce <johnandrewarce@gmail.com>
2019-12-03 02:33:50 +0800

    Updated falcon boilerplate

--- core/commands.py
+++ core/commands.py
@@ -5,5 +5,5 @@ def createproject(project_name):
     """
     A simple method that runs a ManagementUtility.
     """
-    app_clone_script = 'git clone https://github.com/dwm-engineering/falcon-boilerplate.git %s' % project_name
+    app_clone_script = 'git clone https://github.com/jaarce/falcon-bp.git %s' % project_name
     subprocess.call(app_clone_script.split(' '))
